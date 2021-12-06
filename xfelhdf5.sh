3fa7df4e222230b5c085cb5e2f3ebcfb3afba756
Christian Grech <chrisgre23@gmail.com>
2021-12-06 15:11:24 +0100

    daq_parser first version

--- xfelhdf5.sh
+++ xfelhdf5.sh
@@ -9,7 +9,7 @@
 export PYTHONPATH=/beegfs/desy/group/fla/software/daq/libs/CentOS-7-x86_64
 export LD_LIBRARY_PATH=/beegfs/desy/group/fla/software/daq/libs/CentOS-7-x86_64:/beegfs/desy/group/fla/software/daq/libs/CentOS-7-x86_64/extlib
 
-python3 daqraw2hdf5.py -xml xfeldaq_sase2_test.xml -xfel -local -descr xfel_sase1_main_run1727_chan_dscr.xml
+python3 classes/daqraw2hdf5.py -xml temp/filtered_stream_channels.xml -xfel -descr temp/chan_dscr.xml
 
 echo "Job finished - exit"
 exit
